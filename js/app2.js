fa8f5efe4f0115edb71c323fee6a21ca8a97f90f
shovonkhan200h <shovonkhan00@gmail.com>
2023-09-04 01:16:27 +0100

    upload

---
+++ js/app2.js
@@ -0,0 +1,20 @@
+const searchTemperature=()=>{
+    const userInput = document.getElementById(`city-name`).value;
+    fetchData(userInput)
+
+}
+
+
+const fetchData = async(cityName)=>{
+    
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a635628f7603f1bdd3a4c8442f13057d&units=metric`)
+    const data = await res.json()
+    const temp = data.main
+    const city = data.name
+    const temperature = temp.temp
+
+    const cityNames = document.getElementById(`city`).innerHTML = city;
+    const temperatureValue = document.getElementById(`temperature`).innerHTML = temperature;
+    
+    
+}
